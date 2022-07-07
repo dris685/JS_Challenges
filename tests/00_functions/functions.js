facaa32ccb20229c4a479d3540422c5173b27f7f
dris685 <56766548+dris685@users.noreply.github.com>
2022-07-07 01:20:17 -0400

    Update functions.js

--- tests/00_functions/functions.js
+++ tests/00_functions/functions.js
@@ -1,18 +1,42 @@
 /* eslint-disable consistent-return */
-function max() {
-
+function max(num1 = 0, num2 = 0, num3 = 0) {
+  if(num1 >= num2 && num1 >= num3){
+    maxNum = num1
+  }else if(num2 >= num1 && num2 >= num3){
+    maxNum = num2
+  }else{
+    maxNum = num3
+  }
+  return maxNum
 }
 
-function addUnknown() {
-
+function addUnknown(num1 = 0, num2 = 0, num3 = 0, num4 = 0, num5 = 0) {
+  sum = num1 + num2 + num3 + num4 + num5
+  return sum
 }
 
-function evenOrOdd() {
-
+function evenOrOdd(num) {
+  if(num % 2 === 0){
+    evenOrOddNum = "even"
+  }else{
+    evenOrOddNum = "odd"
+  }
+  return evenOrOddNum
 }
 
-function operator() {
-
+function operator(num1 = 0, num2 = 0, num3 = 0) {
+  if(num1 + num2 === num3){
+    operatorSign = "plus"
+  }else if(num1 - num2 === num3){
+    operatorSign = "minus"
+  }else if(num1 * num2 === num3){
+    operatorSign = "multiply"
+  }else if(num1 / num2 === num3){
+    operatorSign = "divide"
+  }else{
+    operatorSign = "null"
+  }
+  return operatorSign
 }
 
 module.exports = {
