a9ad54a84441093fc55cb4ab638891eb53e896f0
dris685 <56766548+dris685@users.noreply.github.com>
2022-07-07 01:22:32 -0400

    Update looping.js

--- tests/02_looping/looping.js
+++ tests/02_looping/looping.js
@@ -1,19 +1,41 @@
 /* eslint-disable no-param-reassign */
 /* eslint-disable no-unused-vars */
 function factorial(num) {
-
+  factorialResult = 1
+  for(i = num; i >= 1; i--){
+      factorialResult = factorialResult * i
+  }
+  return factorialResult
 }
 
 function stringReverse(str) {
-
+  strReverse = ""
+  for(i = str.length-1; i >= 0; i--){
+      strReverse = strReverse + str.charAt(i)
+  }
+  return strReverse
 }
 
-function slicer(str, startIdx, endIdx = str.length - 1) {
-
+function slicer(str, startIdx = 0, endIdx = str.length - 1) {
+  sliceStr = ""
+  for(i = startIdx; i <= endIdx; i++){
+      sliceStr = sliceStr + str.charAt(i)
+  }
+  return sliceStr
 }
 
 function addTheEvens(num) {
-
+  sumEven = 0
+    if(num % 2 != 0){
+        for(i = num-1; i >= 0; i = i-2){
+            sumEven = sumEven + i
+        }
+    }else{
+        for(i = num; i >= 0; i = i-2){
+            sumEven = sumEven + i
+        }
+    }
+    return sumEven
 }
 
 module.exports = {
